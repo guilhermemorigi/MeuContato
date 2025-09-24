9493af189d13e56ac695ce238bc9e0fe59918fd2
guilhermemorigi <gmorigi01@gmail.com>
2025-09-23 21:27:49 -0300

    ajusta conexão no render

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
-import express from "express"
-import userRoutes from "./routes/users.js"
-import cors from "cors"
+import cors from "cors";
+import express from "express";
+import userRoutes from "./routes/users.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use("/", userRoutes)
+app.use("/", userRoutes);
 
-app.listen(8800)
+const PORT = process.env.PORT || 8800;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
